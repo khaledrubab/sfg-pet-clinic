901b54fdfc47d97250abaa540a2aa1bee8efad78
9Z29FC2-PC\rkhaled <rkhaled@tistatech.com>
2018-09-22 01:42:44 -0400

    listed all owners in index page. fixed some bugs too. closes #16

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -23,15 +23,15 @@ public class DataLoader implements CommandLineRunner {
 
         Owner owner1 = new Owner();
         owner1.setId(1L);
-        owner1.setFirstname("Michael");
-        owner1.setLastname("Weston");
+        owner1.setFirstName("Michael");
+        owner1.setLastName("Weston");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setId((2L));
-        owner2.setFirstname("Fiona");
-        owner2.setLastname("Glenanna");
+        owner2.setFirstName("Fiona");
+        owner2.setLastName("Glenanna");
 
         ownerService.save(owner2);
 
@@ -39,15 +39,15 @@ public class DataLoader implements CommandLineRunner {
 
         Vet vet1 = new Vet();
         vet1.setId(1L);
-        vet1.setFirstname("Sam");
-        vet1.setLastname("Axe");
+        vet1.setFirstName("Sam");
+        vet1.setLastName("Axe");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
         vet2.setId(2L);
-        vet2.setFirstname("Jessie");
-        vet2.setLastname(("Porter"));
+        vet2.setFirstName("Jessie");
+        vet2.setLastName(("Porter"));
 
         vetService.save(vet2);
 
